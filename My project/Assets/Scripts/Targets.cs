cf4e1a4ab1c4f28bd48812fa5ede7f26f3cfeb13
Dariaa33 <vesnayoon@gmail.com>
2024-10-24 10:42:30 +0000

    JuegoWestern

---
+++ My project/Assets/Scripts/Targets.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject[] target;
+    float timer = 60;
+    [SerializeField]
+    TextMeshProUGUI timetxt;
+    
+    void Start()
+    {
+        
+        
+       
+    }
+
+
+    void Update()
+    {
+     
+
+    }
+    
+}
